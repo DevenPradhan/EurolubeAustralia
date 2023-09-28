e5fd9fd0f9dc3cc50a30340ba739e5b9c1f132b2
MarketingDMB <marketing@dmbfluidtech.com.au>
2023-09-28 16:33:29 +0800

    end 28 Sept

--- app/Http/Livewire/VisitorProducts.php
+++ app/Http/Livewire/VisitorProducts.php
@@ -16,46 +16,66 @@ class VisitorProducts extends Component
     public $search = '';
     public $categoryId;
     public $categoryCount;
+    public $searchedCategory;
+    public $subCategories;
 
     protected $listeners = ['category-products' => 'mount'];
 
     public function getProducts($id)
     {
+        
+        $this->reset('subCategories');
 
-        if ($this->categoryId) {
-            if ($this->categoryId == $id) {
-                $this->categoryId = '';
+        if ($this->categoryId) { // if categoryId is already set check if it matches
+            if ($this->categoryId == $id) { //if categoryId is equal to the parameter
+                $this->categoryId = ''; //reset the categoryId
             } else {
-                $this->categoryId = $id;
+                $this->categoryId = $id; //else set categoryId to the parameter
             }
         } else {
-            $this->categoryId = $id;
+            $this->categoryId = $id; // if unset categoryId set it to parameter
         }
 
-        $this->categoryCount = ProductCategory::where('referencing', $id)
-            ->orWhere('id', $id)
-            ->get('id');
+        $this->subCategories = ProductCategory::where('referencing', $id)->get();
 
+        if($this->categoryId == '')
+        {
+            $this->resetPage();
+            $this->reset('searchedCategory');
+        }
 
+        if ($this->subCategories->count() == 0) {
+            
+            $this->searchedCategory = $this->categoryId;
+        }
+
+    }
+
+    public function getProductSub($id)
+    {
+        $this->resetPage();
+        $this->reset('searchedCategory');
+        $this->searchedCategory = $id;
+    }
+    public function updatedSearch()
+    {
+        $this->resetPage();
     }
 
     public function render()
     {
+
+        // $products = Product::where('status', 1)->where('name', 'like', '%'. $this->search . '%')->paginate(9);
+        
         $products = Product::query();
 
-        $products->where('name', 'like', '%' .$this->search .'%');
-        // if($this->search){
-        //     $products->where('name', 'like', '%'.$this->search. '%')->whereIn('category_id', $this->categoryCount);
-        // }
-        // else{
-        //     if($this->categoryId){
-        //         $products->whereIn('category_id', $this->categoryCount);
+        $products->where('status', 1);
 
-        //     }
-            
-        // }
+        if ($this->searchedCategory) {
+            $products->where('category_id', $this->searchedCategory);
+        }
 
-        $products = $products->where('status', 1)->paginate(9);
+        $products = $products->where('name', 'like', '%' . $this->search . '%')->paginate(9);
 
         $categories = ProductCategory::where('level', 1)->pluck('name', 'id');
 
--- resources/views/livewire/visitor-products.blade.php
+++ resources/views/livewire/visitor-products.blade.php
@@ -1,18 +1,19 @@
 <div class="flex flex-col h-full justify-center">
     <div class="flex flex-row justify-between space-x-5 h-screen">
-        <div class="w-full grid grid-flow-row md:grid-cols-3 gap-8">
+        <div class="w-full grid grid-flow-row md:grid-cols-3 gap-8 place-content-start">
             @forelse ($products as $product)
-                <a href="{{route('products-each', $product->id)}}" class="h-max w-max">
+                <a href="{{ route('products-each', $product->id) }}" class="h-max w-max">
                     <div
                         class="w-48 sm:w-60  shadow-sm bg-white shadow-gray-300 shrink-0 h-48 sm:h-72 flex flex-col items-center overflow-hidden">
                         <div class="w-full h-40 sm:h-60 overflow-hidden relative">
                             @if ($product->images->count() > 0)
                                 <img src="{{ asset('storage/products/images/' . $product->images->first()->url) }}"
                                     alt="" class="w-full h-full object-scale-down relative z-20 ">
-                                    <img src="{{ asset('images/card bg img.jpg') }}" alt=""
-                                        class="absolute inset-0 opacity-40">
+                                <img src="{{ asset('images/card bg img.jpg') }}" alt=""
+                                    class="absolute inset-0 opacity-40">
                             @else
-                                <div class="p-4 flex items-center w-full h-full bg-gray-200 justify-center text-sm">Image
+                                <div class="p-4 flex items-center w-full h-full bg-gray-200 justify-center text-sm">
+                                    Image
                                     Not found</div>
                             @endif
                         </div>
@@ -33,7 +34,16 @@
                 @foreach ($categories as $key => $category)
                     <x-sidebar-icons type="button"
                         class="{{ $categoryId === $key ? 'bg-red-800 text-white' : 'bg-[#F1F4F5]' }}"
-                        wire:click.prevent="getProducts({{ $key }})">{{ $category }}</x-sidebar-icons>
+                        wire:click.prevent="getProducts({{ $key }})">{{ $category }}
+                    </x-sidebar-icons>
+                    @if ($categoryId === $key)
+                    <div class="-py-3 bg-[#F1F4F5]" >
+                        @foreach ($subCategories as $subCategory)
+                        <button class="{{$searchedCategory === $subCategory->id ? 'bg-red-800 text-white' : 'bg-[#F1F4F5]' }} font-medium text-xs px-2 sm:px-4 py-1.5 sm:py-2.5  whitespace-nowrap w-full hover:bg-red-800 hover:text-[#F1F4F5] transition-colors duration-150 flex tracking-wide"
+                                wire:click.prevent="getProductSub({{$subCategory->id}})">{{$subCategory->name}}</button>
+                        @endforeach
+                    </div>
+                    @endif
                 @endforeach
             </div>
         </div>
@@ -41,7 +51,7 @@
     </div>
 
     <div class=" justify-self-end">
-        {{$products->links()}}
+        {{ $products->links() }}
 
     </div>
 </div>
